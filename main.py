e92324017801c5972fdb5deb58a6836b6e72f377
sitaram <sitaramshelke@gmail.com>
2015-03-03 12:00:23 +0530

    added exception handling for offline network

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 import urllib2
+from urllib2 import Request,urlopen,URLError
 import sys
 from PyQt4 import QtGui,QtCore
+global flag
+flag = 0
 def getword():
+    
     def find_start(h, sstring, estring):
-        substr = sstring
+	        
+	substr = sstring
         index_of_start = html.find(substr)
         newsubstr = html[index_of_start:index_of_start + 100]
         index_of_end = newsubstr.find(estring)
@@ -12,8 +17,22 @@ def getword():
         return substr
 
     request = urllib2.Request('http://www.merriam-webster.com/word-of-the-day/')
-    response = urllib2.urlopen(request)
-    html = response.read()
+    
+    #response = urllib2.urlopen(request)
+    
+    try: 
+        response = urllib2.urlopen(request)
+
+    except URLError, e:
+        print e.reason
+        print "brrrr brrr"
+	global flag        
+	flag = 1
+        print "flag= ",flag 
+    
+    html = ""
+    if(flag==0):
+    	html = response.read()
     # print html
 
     print find_start(html, "Word of the Day:", ">")
@@ -33,13 +52,17 @@ def getword():
     return word,meaning
 
 def main():
+    global flag	
     word,meaning = getword()
-    
+    print "flag = ",flag
+    #word,meaning = "brr","brrrrrrbrrrrrrbrrrrrrbrrrrrrbrrrrrr<br>brrrrrrbrrrrrrbrrrrrrbrrrrrrbrrrrrrbrrrrrr<br>brrrrrrbrrrrrrbrrrrrrbrrrrrrbrrrrrrbrrrrrrbrrrrrr<"
     app = QtGui.QApplication(sys.argv)
 
+    meaning = meaning[:200]+"<br>"+meaning[200:]    
+
     w = QtGui.QWidget()
-    w.resize(500, 200)
-    w.move(400, 300)
+    w.resize(500, 300)
+    w.move(400, 200)
     w.setWindowTitle('Word of the Day')
     w.show()
     
@@ -48,24 +71,35 @@ def main():
     label.move(120,20)
     label.show()
     
-    
-    label1 = QtGui.QLabel("<b>Today's "+word+"</b>")
-    label1.setParent(w)
-    label1.move(100,60)
-    label1.show()
-    
-    label2 = QtGui.QLabel("Meaning: <i>"+meaning+"</i>")
-    label2.setParent(w)
-    label2.move(100,90)
-    label2.show()
-    
+    if(flag==0):
+    	label1 = QtGui.QLabel("<b>Today's "+word+"</b>")
+    	label1.setParent(w)
+    	label1.move(100,60)
+    	label1.show()
+    	
+    	label2 = QtGui.QLabel("<b>Meaning:</b> <i>"+meaning+"</i>")
+    	label2.setParent(w)
+    	label2.move(100,90)
+    	#label2.setMaximumWidth(200)
+    	#label2.setMaximumHeight(300)
+    	label2.setGeometry(100,90,300,100)
+    	#label2.adjustSize()
+    	label2.setWordWrap(True)
+    	label2.show()
+   	 
+    	
+    else:
+    	label1 = QtGui.QLabel("<b>Sorry but you are not connected to internet!!<br>Better Luck next Time :)")
+    	label1.setParent(w)
+    	label1.move(100,90)
+    	label1.show()
+
     bt = QtGui.QPushButton("Close")
     bt.setParent(w)
-    bt.move(200,140)
+    bt.move(200,240)
     bt.show()
     bt.clicked.connect(QtCore.QCoreApplication.instance().quit)
     
-    
     sys.exit(app.exec_())   
 if __name__ == '__main__':
-    main()
+   	 main()
